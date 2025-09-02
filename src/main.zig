0922968c7eb42e2d4a2f51aeaf84364189ec98f2
LuEklund <luckeeklund@gmail.com>
2025-09-02 20:52:19 +0300

    added draw for the game

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const rl = @import("raylib");
 const std = @import("std");
 const builtin = @import("builtin");
-const GameState = @import("game.zig").GameState;
+const GameState = @import("game.zig").State;
 
 const fps = 60;
-const screen_width = 640;
-const screen_height = 480;
+const screen_width = 600;
+const screen_height = 600;
 
 // 💩💩💩💩💩 <-- ga
 const Platform = struct {};
@@ -18,29 +18,35 @@ pub fn main() !void {
     var lib = try std.DynLib.open("zig-out/lib/libgame" ++ comptime builtin.target.dynamicLibSuffix());
     defer lib.close();
     const update = lib.lookup(*const fn (f32, *GameState) callconv(.c) void, "update") orelse return error.LookupFailed;
+    const draw = lib.lookup(*const fn (*GameState, [*]u32) callconv(.c) void, "draw") orelse return error.LookupFailed;
 
     rl.InitWindow(screen_width, screen_height, "ZigGame");
     rl.SetTargetFPS(fps);
-    // var framebuffer: rl.Image = .{
-    //     .width = rl.GetRenderWidth(),
-    //     .height = rl.GetRenderHeight(),
-    //     .format = rl.PixelFormat,
-    //     .mipmaps = 1,
-    //     // .data = // idk the shit from the rasterizer
-    // };
+    var buffer = std.mem.zeroes([screen_width * screen_height]u32);
+    const image: rl.Image = .{
+        .width = rl.GetRenderWidth(),
+        .height = rl.GetRenderHeight(),
+        .format = rl.PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+        .mipmaps = 1,
+        .data = &buffer,
+    };
 
     while (!rl.WindowShouldClose()) {
         const dt = rl.GetFrameTime();
-
-        rl.BeginDrawing();
-        rl.ClearBackground(rl.RAYWHITE);
-
-        std.debug.print("curr state: {any}\n", .{state});
         update(dt, &state);
 
-        rl.DrawRectangleRec(.{ .x = state.pos_x, .y = state.pos_y, .width = 10, .height = 10 }, @bitCast(@as(u32, 0xFF0000FF)));
+        @memset(&buffer, @bitCast(rl.GREEN));
+        draw(&state, &buffer);
+        const texture = rl.LoadTextureFromImage(image);
+        defer rl.UnloadTexture(texture);
 
-        rl.DrawText("Why are you a c enjoyer?", 90, 90, 50, .{ .r = 255, .g = 0, .b = 0, .a = 255.0 });
+        rl.BeginDrawing();
+        rl.ClearBackground(rl.BLACK);
+
+        std.debug.print("curr state: {any}\n", .{state});
+        rl.DrawTexture(texture, 0, 0, rl.WHITE);
+
+        rl.DrawText("Suscribe", 10, screen_height, 30, .{ .r = 255, .g = 0, .b = 0, .a = 255.0 });
         rl.EndDrawing();
     }
 }
