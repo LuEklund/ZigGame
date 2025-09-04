302f2c0ce97df54dcf67e0ef9c3a1e6282033485
LuEklund <luckeeklund@gmail.com>
2025-09-04 21:40:56 +0300

    added recording

--- src/game.zig
+++ src/game.zig
@@ -21,16 +21,15 @@ pub const Pixel = extern struct {
     a: u8 = 0,
 };
 
-pub export fn processInput(state: *State, input: *Input) void {
+//TODO: COPY the state dont use the same!
+pub export fn update(dt: f32, state: *State, input: *Input) void {
     state.dir_y = 0;
     state.dir_x = 0;
     state.dir_y += if (input.w) -1 else 0;
     state.dir_y += if (input.s) 1 else 0;
     state.dir_x += if (input.a) -1 else 0;
     state.dir_x += if (input.d) 1 else 0;
-}
 
-pub export fn update(dt: f32, state: *State) void {
     state.elapsed_time += dt;
     state.pos_x += state.dir_x * dt * 50;
     state.pos_y += state.dir_y * dt * 50;
@@ -41,7 +40,11 @@ pub export fn update(dt: f32, state: *State) void {
 pub export fn draw(state: *State, buffer: [*]Pixel) void {
     for (@intFromFloat(state.pos_y)..@as(usize, @intFromFloat(state.pos_y)) + 10) |y| {
         for (@intFromFloat(state.pos_x)..@as(usize, @intFromFloat(state.pos_x)) + 10) |x| {
-            buffer[x + y * 600] = @bitCast(@as(u32, 0xFF0000FF));
+            // buffer[x + y * 600] = @bitCast(@as(u32, 0xFF0000FF));
+            buffer[x + y * 600].r = @intCast(@mod(@as(u32, @intFromFloat(state.elapsed_time * 50)), 0xAA));
+            buffer[x + y * 600].g = @intCast(@mod(@as(u32, @intFromFloat(state.elapsed_time * 0)), 0xFF));
+            buffer[x + y * 600].b = @intCast(@mod(@as(u32, @intFromFloat(state.elapsed_time * 50)), 0xFF));
+            buffer[x + y * 600].a = 0xFF;
         }
     }
 }
